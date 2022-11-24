4185e2a7bff7596d00d1e8f9e3420f0bea67bbec
dean_hsu <dean_hsu@paradise-soft.com.tw>
2022-11-24 11:10:21 +0800

    rename pkg

--- cmd/root.go
+++ cmd/root.go
@@ -16,7 +16,7 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "arangodb-repo-gen",
+	Use:   "helper-commands",
 	Short: "A brief description of your application",
 	Long: `A longer description that spans multiple lines and likely contains
 examples and usage of using your application. For example:
