122e705449b7fa9e387948c4764cc505014b77cf
dean_hsu <dean_hsu@paradise-soft.com.tw>
2022-11-24 11:01:40 +0800

    feat: new command for create migration file

---
+++ cmd/collectionMigration.go
@@ -0,0 +1,88 @@
+/*
+Copyright © 2022 Dean_Hsu <jasugun0000@gmail.com>
+
+*/
+package cmd
+
+import (
+	"errors"
+	"fmt"
+	"github.com/DeanXu2357/arangodb-repo-gen/tpl"
+	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"text/template"
+	"time"
+)
+
+// collectionMigrationCmd represents the collectionMigration command
+var collectionMigrationCmd = &cobra.Command{
+	Use:   "collectionMigration [collection name]",
+	Short: "撰寫 migration 新增 collection",
+	Long:  `撰寫 migration 新增 collection`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("collectionMigration called")
+
+		g := NewCreateCollectionGenerator(args[0])
+
+		cobra.CheckErr(g.Create())
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(collectionMigrationCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// collectionMigrationCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// collectionMigrationCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+type createColGenerator struct {
+	path           string
+	version        string
+	collectionName string
+}
+
+func NewCreateCollectionGenerator(colName string) *createColGenerator {
+	version := time.Now().Format("20060102")
+
+	fileName := fmt.Sprintf("%s_Create_%s_Collection.js", version, colName)
+
+	root, err := os.Getwd()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &createColGenerator{
+		path:           fmt.Sprintf("%s/migrations/%s", root, fileName),
+		version:        version,
+		collectionName: colName,
+	}
+}
+
+func (g *createColGenerator) Create() error {
+	if _, err := os.Stat(g.path); os.IsExist(err) {
+		return errors.New("file already exist")
+	}
+
+	cmdFile, err := os.Create(g.path)
+	if err != nil {
+		return err
+	}
+	defer cmdFile.Close()
+
+	commandTemplate := template.Must(
+		template.New("sub").Parse(string(tpl.MigrationCreateTemplate())),
+	)
+	err = commandTemplate.Execute(cmdFile, g)
+	if err != nil {
+		return err
+	}
+	return nil
+}
---
+++ tpl/migration_tpl.go
@@ -0,0 +1,84 @@
+package tpl
+
+func MigrationCreateTemplate() []byte {
+	return []byte(`
+const _ = require('lodash');
+const { logger } = require('.');
+const currentVersion = '{{ .version }}';
+
+module.exports.Execute = async (db, aql, logger, env) => {
+    try {
+        const defaultCol = db.collection('{{ .colName }}');
+
+        const hasDefaultCol = await defaultCol.exists();
+
+
+        // ensure collections
+        if (!hasDefaultCol) {
+            await db.createCollection('{{ .colName }}')
+        }
+
+        const cursor = await db.query(aql` + "`" + `
+	FOR v IN Versions
+	FILTER !IS_NULL(v.migrationNumber) AND
+	v.migrationNumber == ${currentVersion}
+	RETURN v._key
+	` + "`" + `);
+        const currentMigration = await cursor.next();
+        if (!_.isUndefined(currentMigration)) {
+            return;
+        }
+
+        // update the schema version to {{ .version }}
+        logger.info('update the schema version the {{ .version }}');
+        await db.collection('Versions').save(
+            {
+                migrationNumber: currentVersion,
+                createdAt: Date.now(),
+            },
+            { returnNew: true },
+        );
+        logger.info('*** migration successfully execution ***');
+        return;
+    } catch (error) {
+        console.log('shit gg :\n', error);
+    }
+}
+
+module.exports.Rollback = async (db, aql, logger, env) => {
+    try {
+        const versionsCol = db.collection('Versions');
+        const cursor = await db.query(aql` + "`" + `
+	FOR v IN Versions
+	FILTER !IS_NULL(v.migrationNumber) AND
+	v.migrationNumber == ${currentVersion}
+	RETURN v._key
+	` + "`" + `);
+        const currentMigration = await cursor.next();
+
+        await dropCollection(db, '{{ .colName }}');
+
+        // 移除 Migration
+        await versionsCol.remove(currentMigration);
+
+        logger.info('*** rollback successfully execution ***');
+        return;
+    } catch (error) {
+        console.log('Rollback Error:\n', error);
+    }
+}
+
+const dropCollection = async (db, colName) => {
+    const collection = db.collection(colName);
+    const hasCol = await collection.exists();
+
+    if (!hasCol) {
+        logger.info('Collection %s not exists', colName);
+    } else {
+        collection.drop();
+        logger.info('Drop %s collection success', colName);
+    }
+    return;
+};
+`)
+}
